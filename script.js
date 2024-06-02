f7e9d33e7b3dca121dec0a62cc2557498b4a8809
xVc323 <41209493+xVc323@users.noreply.github.com>
2024-06-03 01:17:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const text = "hey, this is my website :)";
+const text = "hey, this is my website. stay tuned for more updates :)";
 let index = 0;
 const speed = 100; // typing speed (ms)
 const typingText = document.getElementById('typingText');
